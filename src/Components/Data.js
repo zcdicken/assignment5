7e03f70fae5c0c8e5091f2b4711846edb01f912b
zcdicken <89824899+zcdicken@users.noreply.github.com>
2021-10-05 00:45:05 -0400

    Add files via upload

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 
 function Data() {
-    const [data] = React.useState([{name:"Bob", age:"19", color:"Teal", id:"1"}, {name:"Hammy", age:"67", color:"Orange", id:"2"}, {name:"Bigface", age:"26", color:"Red", id:"3"}]);
-
     return (
         <div>
             <p>Name:Bob | age: 19 | color: Teal | id: 1</p>
